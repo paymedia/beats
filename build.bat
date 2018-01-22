4ac2c104bdc4e382f89ad30ea120fd18fa34e1e8
paymedia <Aleksandar.Manojlovic@hsntech.com>
2018-01-22 13:37:51 -0800

    i40985 Modified build
    Modified build script so it zips the artifacts and can build under a root\src directory in Team City.

--- build.bat
+++ build.bat
@@ -1,2 +1,18 @@
+set GOPATH=c:\
 cd winlogbeat
 go.exe build
+..\7z.exe a winlogbeat.zip winlogbeat.exe -r
+..\7z.exe a winlogbeat.zip winlogbeat.yml
+..\7z.exe a winlogbeat.zip fields.yml
+..\7z.exe a winlogbeat.zip kibana\default\dashboard\Winlogbeat-overview.json
+..\7z.exe a winlogbeat.zip kibana\default\index-pattern\winlogbeat.json
+..\7z.exe a winlogbeat.zip kibana\5.x\dashboard\Winlogbeat-Dashboard.json
+..\7z.exe a winlogbeat.zip kibana\5.x\index-pattern\winlogbeat.json
+..\7z.exe a winlogbeat.zip kibana\5.x\search\Winlogbeat-Search.json
+..\7z.exe a winlogbeat.zip kibana\5.x\visualization\Errors-count-over-time.json
+..\7z.exe a winlogbeat.zip kibana\5.x\visualization\Event-Levels.json
+..\7z.exe a winlogbeat.zip kibana\5.x\visualization\Evolution-of-the-CPU-times-per-process.json
+..\7z.exe a winlogbeat.zip kibana\5.x\visualization\Number-of-Events.json
+..\7z.exe a winlogbeat.zip kibana\5.x\visualization\Number-of-Events-Over-Time-By-Event-Log.json
+..\7z.exe a winlogbeat.zip kibana\5.x\visualization\Sources.json
+..\7z.exe a winlogbeat.zip kibana\5.x\visualization\Top-Event-IDs.json
